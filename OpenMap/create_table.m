4ab1d9c6bbdbba48389aafae51d0b5ee518e470f
suhaib123 <suhaib.a@hotmail.com>
2014-05-10 17:57:34 -0700

    Update create_table.m

--- OpenMap/create_table.m
+++ OpenMap/create_table.m
@@ -1,1 +1,25 @@
-/**/
+typedef struct _node{
+  NSString *name
+  int contract_count;
+  void* next;
+}node;
+
+node *contractor_array;
+
+for(int i=0; i<arr.count; i++){
+  
+}
+
+find_entry(NSSTring name, node* array){
+  void* next = array;
+  while(next != NULL){
+    if(strcmp((node*)next->name, name)==0)
+      break;
+    next = array->next;
+  }
+  return next;
+}
+
+add_entry(NSString){
+
+}
